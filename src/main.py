38a0073aa3633cf6432d3ac735b93fc6ec8f3eea
nodokaha <scpeee@wail.ch>
2025-08-16 02:14:59 +0900

    'update'

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from pythonosc import osc_server
 import flet as ft
 import random
 
-wand_sound = {"C":[0,4], "C#":[5,8], "D":[9,12], "D#":[13,15], "E":[16,20], "F":[21,24], "F#":[25, 28], "G":[29,32], "G#":[33,36], "A":[37, 40], "A#":[41,44], "B":[45,48]}
+wand_sound = {"C":[0,4], "C#":[5,8], "D":[9,12], "D#":[13,15], "E":[16,20], "F":[21,24], "F#":[25, 28], "G":[29,32], "G#":[33,36], "A":[37, 40], "A#":[41,44], "B":[45,48], "C_hi":[49,52], "C#_hi":[53,56], "D_hi":[57,60], "D#_hi":[61,64], "E_hi":[65,68], "F_hi":[69,72], "F#_hi":[73,76], "G_hi":[77,80], "G#_hi":[81,84], "A_hi":[85,88], "A#_hi":[89,92], "B_hi":[93,96], "C_hihi":[97,100]}
 song = []
 client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
 
@@ -35,6 +35,19 @@ def main(page: ft.Page):
     A = ft.FilledButton(on_click=insert_bell, text="A")
     A_sharp = ft.FilledButton(on_click=insert_bell, text="A#")
     B = ft.FilledButton(on_click=insert_bell, text="B")
+    C_hi = ft.FilledButton(on_click=insert_bell, text="C_hi")
+    C_sharp_hi = ft.FilledButton(on_click=insert_bell, text="C#_hi")
+    D_hi = ft.FilledButton(on_click=insert_bell, text="D_hi")
+    D_sharp_hi = ft.FilledButton(on_click=insert_bell, text="D#_hi")
+    E_hi = ft.FilledButton(on_click=insert_bell, text="E_hi")
+    F_hi = ft.FilledButton(on_click=insert_bell, text="F_hi")
+    F_sharp_hi = ft.FilledButton(on_click=insert_bell, text="F#_hi")
+    G_hi = ft.FilledButton(on_click=insert_bell, text="G_hi")
+    G_sharp_hi = ft.FilledButton(on_click=insert_bell, text="G#_hi")
+    A_hi = ft.FilledButton(on_click=insert_bell, text="A_hi")
+    A_sharp_hi = ft.FilledButton(on_click=insert_bell, text="A#_hi")
+    B_hi = ft.FilledButton(on_click=insert_bell, text="B_hi")
+    C_hihi = ft.FilledButton(on_click=insert_bell, text="C_hihi")
 
     page.add(
         ft.Text("えるしおんちゃんプレイヤー"),
@@ -42,6 +55,8 @@ def main(page: ft.Page):
             song_view,
             expand=True,
         ),
+        ft.Row(
+        controls=[
                 C,
                 C_sharp,
                 D,
@@ -54,6 +69,25 @@ def main(page: ft.Page):
                 A,
                 A_sharp,
                 B
+                ]
+        ),
+        ft.Row(
+        controls=[
+                C_hi,
+                C_sharp_hi,
+                D_hi,
+                D_sharp_hi,
+                E_hi,
+                F_hi,
+                F_sharp_hi,
+                G_hi,
+                G_sharp_hi,
+                A_hi,
+                A_sharp_hi,
+                B_hi,
+                C_hihi
+                ]
+        )
     )
     dispatcher = Dispatcher()
     dispatcher.map("/avatar/parameters/E&L/Ring", next, "wave")
